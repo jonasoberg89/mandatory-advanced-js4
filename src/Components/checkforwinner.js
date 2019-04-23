7dd06aa05c15e5674a9ce58e188850d1c40a860b
Jonas <folke_89@hotmail.com>
2019-04-23 08:55:20 +0200

    fixed horizontal combo

--- src/Components/checkforwinner.js
+++ src/Components/checkforwinner.js
@@ -3,29 +3,22 @@ let winnerCombosVertical = [
     [1,2,3,4],
     [2,3,4,5],
 ]
-
-function check(array, length) {
-    var count = 0,
-        value = array[0];
-
-    return array.some(function (a) {
-        if (value !== a) {
-            count = 0;
-            value = a;
-        }
-        return ++count === length;
-    });
-}
+let winnerCombosHorizontal = [
+    [0,1,2,3],
+    [1,2,3,4],
+    [2,3,4,5],
+    [3,4,5,6],
+]
 function checkForWinner(array,index,player){
     const testVertical = winnerCombosVertical.find(combo => combo.every(line =>array[index][line]===player)) 
     if(testVertical)return player;
-    
-    for (let i =0;i < 6;i++){
-        const winnerCombosHorizontal = array.map((colum) =>{
-        return colum[i];
+
+    for (let i = 0; i < 6;i++){
+        const horizontal = array.map((colum) =>{
+            return colum[i];
         })
-   
-        console.log( check(winnerCombosHorizontal,4))
+        const testHorizontal = winnerCombosHorizontal.find(combo => combo.every(line =>horizontal[line]===player)) 
+        if(testHorizontal)return player;    
     }
 
  
