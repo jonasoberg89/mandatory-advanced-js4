b87033ea770a3e0f9d2fabc3e2f4be7124e1adfb
jonasoberg89 <folke_89@hotmail.com>
2019-04-26 22:27:17 +0200

    Updates

---
+++ precache-manifest.3632fa7ac7120bb579830eb5db955bf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb147c3d7ae77fe3eefb",
+    "url": "/mandatory-advanced-js4/static/js/runtime~main.78de8069.js"
+  },
+  {
+    "revision": "185d88475e2bcaa8db82",
+    "url": "/mandatory-advanced-js4/static/js/main.dbd93e92.chunk.js"
+  },
+  {
+    "revision": "37a75e0d80732888e7da",
+    "url": "/mandatory-advanced-js4/static/js/2.3393b8b4.chunk.js"
+  },
+  {
+    "revision": "185d88475e2bcaa8db82",
+    "url": "/mandatory-advanced-js4/static/css/main.cb5511c4.chunk.css"
+  },
+  {
+    "revision": "fc8c515c8a140dcae2e6200f6349af80",
+    "url": "/mandatory-advanced-js4/index.html"
+  }
+];
